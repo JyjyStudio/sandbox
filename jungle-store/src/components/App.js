27e2bf139dfa31a31d3f1dd7d7cbdf80f2f2d538
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-18 14:39:40 +0200

    Finito

--- jungle-store/src/components/App.js
+++ jungle-store/src/components/App.js
@@ -1,19 +1,44 @@
-import '../styles/wrapper.css'
-import React from 'react'; 
-import Banner from './Banner';
-import List from './List';
-import Cart from "./Cart";
-import Items from './Card'
+import React, { useState } from "react"
+import Banner from "./Banner"
+import Categories from "./Categories"
+import Cart from "./Cart"
+import Card from "./Card"
+import { plantList } from "../datas/plantList"
+import Footer from "./Footer"
 
-const App = () => (
-    <React.StrictMode>
-		<Banner />
-		<List />
-		<div className='wrapper'>
-			<Cart />
-			<Items />
-		</div>
-    </React.StrictMode>
-);
+const App = () => {
+
+	const [cart, updateCart] = useState([])
+
+	return (
+		<React.StrictMode>
+			<Banner />
+			<div className="container">
+				<Cart cart={cart} updateCart={updateCart} />
+				<div className="content">
+					<Categories />
+					<div className="card-wrapper">
+						{plantList.map(plant => (
+							<Card
+								key={plant.id}
+								name={plant.name}
+								categorie={plant.category}
+								price={plant.price}
+								isBestSale={plant.isBestSale}
+								isSpecialOffer={plant.isSpecialOffer}
+								water={plant.water}
+								light={plant.light}
+								cover={plant.cover}
+								updateCart={updateCart}
+								cart={cart}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</React.StrictMode>
+	)
+}
 
 export default App
