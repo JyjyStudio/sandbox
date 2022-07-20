9fd087494d30a1253fe34e4d667826f389304dca
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-20 09:02:41 +0200

    feat : tri par catégorie

--- jungle-store/src/components/Categories.js
+++ jungle-store/src/components/Categories.js
@@ -1,23 +1,33 @@
-import '../styles/List.css';
-import React from "react";
-import { plantList } from "../datas/plantList";
+import '../styles/List.css'
+import React, { useState } from "react"
+import { plantList } from "../datas/plantList"
 
 
 const Categories = () => {
+
+    const [productList, setProductList] = useState(plantList)
 
     const categoriesList = plantList.reduce((acc, curr) => {
         if(!acc.includes(curr.category)) acc.push(curr.category)
         return acc
     }, [])
 
+    const checkCategory = () => {
+        const selectInput = document.getElementById('categories')
+        const goodCategory = [...productList].filter(product => product.category === selectInput.value)
+        setProductList(goodCategory)
+        console.log({productList});
+        console.log(selectInput.value);
+    }
+
     return (
         <React.StrictMode>
             <label htmlFor="categories">Catégories</label>
-            <select id="categories">
+            <select id="categories" onChange={() => checkCategory()}>
                 <option key='placeholder'>Selectionner une catégorie 🌻</option>
                 { 
                     categoriesList.map((categorie, id) => (
-                        <option key={id}>{categorie}</option>
+                        <option key={id} value={categorie}>{categorie}</option>
                     ))
                 }
             </select>
