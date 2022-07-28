5911b194536730e544f28eb7180cec7ec064dc4f
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-28 10:34:25 +0200

    styled component props

--- jungle-store/src/components/Button.js
+++ jungle-store/src/components/Button.js
@@ -2,13 +2,12 @@ import styled from "styled-components"
 
 const Button = styled.button`
 	padding: 3px;
-	margin-left: ${props => props.ml10 && '10px'};
-	margin-top: ${props => props.mt15 && '15px'};
-	margin-top: ${props => props.mt20 && '20px'};
+	margin: ${props => props.margin};
 	width: ${props => props.large && '100%'};
-	cursor: ${props => props.cursorPointer && 'pointer'};
+	cursor: ${props => props.cursor};
+	display: ${props => props.display};
 	@media screen and (max-device-width : 480px) {
-		${props => props.centerMobile ? 'margin: 10px auto 0' : '0'
+		${props => props.centerMobile && 'margin: 10px auto 0'}
 	}
 `
 
