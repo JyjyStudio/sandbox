049a5895aaaf00d097d4426857732d30ea3b92f4
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-29 21:43:28 +0200

    styled component props

--- shiny-agency/front/src/components/Button.jsx
+++ shiny-agency/front/src/components/Button.jsx
@@ -4,15 +4,16 @@ import colors from "../utils/colors"
 const Button = styled.button`
 	color: ${props => props.color};
 	background: ${props => props.backgroundColor};
-	border-radius: 29px;
+	border-radius: ${props => props.borderRadius ? props.borderRadius : "29px"};
 	padding: ${props => props.padding};
 	margin: ${props => props.margin};
-	border: none;
+	border: ${props => props.border ? props.border : "none"};
 	cursor: pointer;
 	font-size: 17px;
 	&:hover {
 		color: ${colors.primary};
-		border: ${props => props.blueHover && `1px ${colors.blueBackground} solid`}
+		border: ${props => props.blueHover && `1px ${colors.blueBackground} solid`};
+		background-color: ${props => props.hoverBackground};
 	}
 `
 
