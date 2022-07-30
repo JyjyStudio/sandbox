810094ea7bfa868793efec80b13170656f847ac3
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-30 21:44:55 +0200

    borderRadius added

--- shiny-agency/front/src/components/Img.jsx
+++ shiny-agency/front/src/components/Img.jsx
@@ -4,5 +4,6 @@ const Img = styled.img`
 	width: ${props => props.width};
 	height: ${props => props.height};
 	margin: ${props => props.margin};
+	border-radius: ${props => props.borderRadius}
 `
 export default Img
