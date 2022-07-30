51997598005889a474623aabc35df5869459d2fe
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-07-30 21:45:17 +0200

    textAlign et margin

--- shiny-agency/front/src/components/FirstHeading.jsx
+++ shiny-agency/front/src/components/FirstHeading.jsx
@@ -3,7 +3,8 @@ import styled from "styled-components"
 const FirstHeading = styled.h1`
 	width: ${props => props.width};
 	font-size: ${props => props.fontSize};
-	text-align: ${props => props.textAlign}
+	text-align: ${props => props.textAlign};
+	margin: ${props => props.margin};
 `
 
 export default FirstHeading
