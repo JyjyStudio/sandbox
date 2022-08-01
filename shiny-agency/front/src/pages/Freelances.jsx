fdb906d212d84c51dc013e28b2b31bdb9d0c8577
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-01 11:26:17 +0200

    utilisation de l'API plutot que le json mocké

--- shiny-agency/front/src/pages/Freelances.jsx
+++ shiny-agency/front/src/pages/Freelances.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
-import freelances from '../data/freelances'
 import {v4 as uuidv4} from 'uuid'
 import styled from 'styled-components'
 import FirstHeading from '../components/FirstHeading'
 import colors from '../utils/colors'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Freelances = () => {
-  	return (
+
+	const [freelancers, setFreelancers] = useState([])
+
+	useEffect( () => {
+		axios.get('http://localhost:8000/freelances')
+		.then(res => {
+			const freelancersList = res.data
+			setFreelancers(freelancersList['freelancersList'])
+		})
+	}, [])
+	console.log(freelancers);
+	return (
 		<React.StrictMode>
 			<FirstHeading textAlign="center">Trouvez votre prestataire</FirstHeading>
 			<Slogan>Chez Shiny nous réunissons les meilleurs profils pour vous.</Slogan>
-			<CardsContainer display="grid" gridTemplateCol="repeat(auto-fill, 300px)" margin="100px 0 0 0">
-				{ freelances.map(freelance => 
-					<LinkedCards to={'/freelances/' + freelance.name.split(' ').join('-')} key={uuidv4()}>
-						<Card jobTitle={freelance.jobTitle} picture={freelance.picture} name={freelance.name} />
+			<CardsContainer display="grid" gridTemplateCol="repeat(auto-fill, 300px)" margin="100px 0" >
+				{ freelancers.map(freelance => 
+					<LinkedCards to={'/freelances/' + freelance.name.split(' ').join('-')} key={uuidv4()} state={freelance}>
+						<Card jobTitle={freelance.job} picture={freelance.picture} name={freelance.name} />
 					</LinkedCards>
 				)}
 			</CardsContainer>
