8b5e927e177dd7d24249799b6957ade673e0fdcf
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-01 11:26:59 +0200

    utilisation des données de l'API plutot que celles du json mocké

--- shiny-agency/front/src/pages/Freelance.jsx
+++ shiny-agency/front/src/pages/Freelance.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import ArrowImg from '../assets/left-arrow.png'
 import Button from '../components/Button'
 import RectangleContainer from '../components/RectangleContainer'
 import Img from '../components/Img'
 import FirstHeading from '../components/FirstHeading'
+import SecondHeading from '../components/SecondHeading'
+import colors from '../utils/colors'
 
 const Freelance = () => {
+
 	const navigate = useNavigate()
 	const goBack = () => navigate(-1)
-	const params = useParams()
+
+	const location = useLocation()
+  	const freelance  = location.state
+  	console.log('freelance:', freelance)
+
 	return (
 		<React.StrictMode>
 
@@ -21,9 +28,24 @@ const Freelance = () => {
 				</Button>
 			</GoBackContainer>
 
-			<RectangleContainer>
-				<Img src='' alt='profil'/>
-				<FirstHeading textAlign="center">{params.name.split('-').join(' ')}</FirstHeading>
+			<RectangleContainer justifyContent="center">
+				<Img src={freelance.picture} alt='profil' width={150} borderRadius="50%" />
+
+				<Infos>
+					<FirstHeading margin="1em 0 0 0">{freelance.name}</FirstHeading>
+
+					<SecondHeading margin="1em 0">{freelance.job}</SecondHeading>
+
+					<ButtonsContainer>
+						{ freelance.skills.map(skill => <Button key={skill} backgroundColor="none" border="1px black solid" padding="10px" borderRadius="15px" margin="0 15px 25px 0" hoverBackground="#cecece">{skill}</Button>) }
+					</ButtonsContainer>
+
+					<div>{freelance.available ? '🟢 Disponible maintenant' : '🔴 Non disponible actuellement'}</div>
+
+					<SecondHeading>{freelance.tjm}€/jour</SecondHeading>
+				</Infos>
+
+				<Location>{freelance.location}</Location>
 			</RectangleContainer>
 			
 		</React.StrictMode>
@@ -32,7 +54,19 @@ const Freelance = () => {
 
 const GoBackContainer = styled.div`
 	display: flex;
-	margin-left: 50px;
+	margin: 0 0 50px 50px
 `
+const Infos = styled.div`
+	margin: 20px 70px 50px 150px;
+`
+const ButtonsContainer = styled.div`
 
+`
+const Location = styled.div`
+	color: ${colors.secondary};
+    margin: 2.8em 0 0 0;
+    align-self: start;
+    font-size: 1.3em;
+    font-weight: 500;
+`
 export default Freelance
