d0cd5db699f63de24ca3f045ce70cfded86a9f87
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-02 18:11:43 +0200

    feat: footer + ThemeProvider

--- shiny-agency/front/src/pages/App.jsx
+++ shiny-agency/front/src/pages/App.jsx
@@ -6,18 +6,26 @@ import NotFound from '../components/NotFound'
 import Results from './Results'
 import Freelances from './Freelances'
 import Freelance from './Freelance'
+import Footer from '../components/Footer'
+import ThemeProvider from '../utils/Context/Context'
+import GlobalStyle from '../utils/styles/GlobalStyle'
+
 
 const App = () => (
 	<div className="App">
-		<Header />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/questionnaire/:question" element={<Survey />} />
-			<Route path='*' element={<NotFound />} />
-			<Route path='/resultats' element={<Results />} />
-			<Route path='/freelances' element={<Freelances />} />
-			<Route path='/freelances/:name' element={<Freelance />} />
-		</Routes>
+		<ThemeProvider>
+			<GlobalStyle/>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/questionnaire/:questionNumber" element={<Survey />} />
+				<Route path='*' element={<NotFound />} />
+				<Route path='/resultats' element={<Results />} />
+				<Route path='/freelances' element={<Freelances />} />
+				<Route path='/freelances/:name' element={<Freelance />} />
+			</Routes>
+			<Footer/>
+		</ThemeProvider>
 	</div>
 )
 
