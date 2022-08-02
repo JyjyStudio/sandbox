4037c2c1209309f7429d6b3d329da03a37b6d7b3
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-02 18:10:52 +0200

    logo change en fonction du darkmode (useContext)

--- shiny-agency/front/src/components/Header.jsx
+++ shiny-agency/front/src/components/Header.jsx
@@ -1,23 +1,32 @@
-import React from "react"
-import styled from "styled-components"
-import Navbar from "./Navbar"
-import Img from "./Img"
-import LogoSrc from '../assets/dark-logo.png'
-import { Link } from "react-router-dom"
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import Navbar from './Navbar'
+import Img from './Img'
+import DarkLogo from '../assets/dark-logo.png'
+import LightLogo from '../assets/light-logo.png'
+import { Link } from 'react-router-dom'
+import { ThemeContext } from '../utils/Context/Context'
 
 const Header = () => {
-  return (
-	<HeaderContainer>
-		<Link to="/">
-			<Img src={LogoSrc} width="190px" height="70px" margin="30px" alt="logo"/>
-		</Link>
-		<Navbar />
-	</HeaderContainer>
-  )
+	const {theme} = useContext(ThemeContext)
+	return (
+		<HeaderContainer>
+			<Link to="/">
+				<Img
+					src={theme === 'light' ? DarkLogo : LightLogo}
+					width="190px"
+					height="70px"
+					margin="30px"
+					alt="logo"
+				/>
+			</Link>
+			<Navbar />
+		</HeaderContainer>
+	)
 }
 
 const HeaderContainer = styled.header`
-	display:flex;
+	display: flex;
 	align-item: center;
 	justify-content: space-between;
 	font-size: calc(10px + 2vmin);
