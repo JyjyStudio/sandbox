5e8a5be70dd148e923f0d55a13ba5b246f88de88
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-03 14:55:19 +0200

    SurveyProvider added

--- shiny-agency/front/src/pages/App.jsx
+++ shiny-agency/front/src/pages/App.jsx
@@ -7,24 +7,26 @@ import Results from './Results'
 import Freelances from './Freelances'
 import Freelance from './Freelance'
 import Footer from '../components/Footer'
-import ThemeProvider from '../utils/Context/Context'
+import {SurveyProvider, ThemeProvider} from '../utils/Context/Context'
 import GlobalStyle from '../utils/styles/GlobalStyle'
 
 
 const App = () => (
 	<div className="App">
 		<ThemeProvider>
-			<GlobalStyle/>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/questionnaire/:questionNumber" element={<Survey />} />
-				<Route path='*' element={<NotFound />} />
-				<Route path='/resultats' element={<Results />} />
-				<Route path='/freelances' element={<Freelances />} />
-				<Route path='/freelances/:name' element={<Freelance />} />
-			</Routes>
-			<Footer/>
+			<SurveyProvider>
+				<GlobalStyle/>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/questionnaire/:questionNumber" element={<Survey />} />
+					<Route path='*' element={<NotFound />} />
+					<Route path='/resultats' element={<Results />} />
+					<Route path='/freelances' element={<Freelances />} />
+					<Route path='/freelances/:name' element={<Freelance />} />
+				</Routes>
+				<Footer/>
+			</SurveyProvider>
 		</ThemeProvider>
 	</div>
 )
