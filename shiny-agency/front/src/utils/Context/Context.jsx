56cd5530c73b20ef514effe7ed0a59e077e6cc7f
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-03 14:53:15 +0200

    feat: SurveyContext

--- shiny-agency/front/src/utils/Context/Context.jsx
+++ shiny-agency/front/src/utils/Context/Context.jsx
@@ -1,15 +1,28 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from 'react'
 
 export const ThemeContext = createContext()
-
-export default function ThemeProvider( {children} ) {
+export function ThemeProvider({ children }) {
 	const [theme, setTheme] = useState('light')
 
 	const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
-	
+
 	return (
-		<ThemeContext.Provider value={{theme, toggleTheme}}>
+		<ThemeContext.Provider value={{ theme, toggleTheme }}>
 			{children}
 		</ThemeContext.Provider>
+	)
+}
+
+export const SurveyContext = createContext()
+export function SurveyProvider({ children }) {
+
+	const [answers, setAnswers] = useState({})
+
+	const saveAnswers = (newAnswers) => setAnswers({ ...answers, ...newAnswers })
+
+	return (
+		<SurveyContext.Provider value={{ answers, saveAnswers }}>
+			{ children }
+		</SurveyContext.Provider>
 	)
 }
