f3cbeefd1736ab8820e41398fa1f1cab3831adea
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-01 10:34:55 +0200

    ajout du Provider

--- todolist-redux-toolkit/src/index.jsx
+++ todolist-redux-toolkit/src/index.jsx
@@ -1,12 +1,15 @@
-import React, { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import '@picocss/pico'
+import { store } from './utils/redux'
 import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<StrictMode>
-		<Home />
+		<Provider store={store}>
+			<Home />
+		</Provider>
 	</StrictMode>
 )
